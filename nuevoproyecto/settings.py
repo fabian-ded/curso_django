6b92989d3852b3edc43a2394fd17849341d83b17
favian silva <fabiansil902@gmail.com>
2025-06-08 12:04:02 -0500

    ademas vi como se creaban las clases de base de datos que se creaban en la aplicacion

--- nuevoproyecto/settings.py
+++ nuevoproyecto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store' #aqui se coloca la aplicacion para que el proyecto lo pueda reconocer y ademas conocer las clases que tenga este
 ]
 
 MIDDLEWARE = [
--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    
+class Task(models.Model):
+    title = models.CharField(max_length=100)#para un numero de textos permitidos escribir
+    description = models.TextField()#para textos mucho mas largos
+    Project = models.ForeignKey(Project, on_delete=models.CASCADE)#Aqui se hace la relacion de las dos clases o mejor dicho de las dos tablas
+    #ademas utilizamos la funcion de cascada que hace que si se elimina un proyecto se eliminen los datos que se tienen relacionados a ese
+    
+    
