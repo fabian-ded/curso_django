9d5337680884fb27620ea7ffcbbcd18deb7d92b0
favian silva <fabiansil902@gmail.com>
2025-06-09 14:47:22 -0500

    estuve biend el el curso como se reciben parametros en la url de la pagina

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     #path("admin/":'como se debe llamar en la url para que se pueda ver', "admin.site.urls":la ruta donde debe buscar django para mostrar si el usuario busco por esa url)
-    path('', views.Hola),
-    path('estado/', views.estado) 
+    path('', views.index),
+    path('estado/', views.estado),
+    path('Hola/<str:username>', views.Hola)#ese "<str:username>" funciona para esperar un parametro que nos de el usuario y al recibirlo mostrar el contenido que se debe enviar
 ]
--- store/views.py
+++ store/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def Hola(request):
-    return HttpResponse("<h1>Hola Mundo</h1>")
+def Hola(request, username):#aqui el username es el dato que debe reciir para funcionar esta funcion
+    print(username)#se muestra el username que ingreso el usuario mediante la url
+    return HttpResponse("<h1>Hola %s</h1>" %username)
 
 def estado(request):
     return HttpResponse("<h2>Comó estás?</h2>")
+
+def index(request):
+    return HttpResponse("index page")
