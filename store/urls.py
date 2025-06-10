b6a6daae084b132ef532602c7720a83b4cd9907f
favian silva <fabiansil902@gmail.com>
2025-06-10 15:28:29 -0500

    sigo con el curso esta vez vi como buscar datos en la base de datos mediante los parametros que tiene cada funcion

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     #path("admin/":'como se debe llamar en la url para que se pueda ver', "admin.site.urls":la ruta donde debe buscar django para mostrar si el usuario busco por esa url)
     path('', views.index),
     path('estado/', views.estado),
-    path('Hola/<str:username>', views.Hola)#ese "<str:username>" funciona para esperar un parametro que nos de el usuario y al recibirlo mostrar el contenido que se debe enviar
+    path('Hola/<str:username>', views.Hola),#ese "<str:username>" funciona para esperar un parametro que nos de el usuario y al recibirlo mostrar el contenido que se debe enviar
+    path('projects/', views.projects),
+    path('tasks/<int:id>', views.tasks),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def Hola(request, username):#aqui el username es el dato que debe reciir para funcionar esta funcion
@@ -11,3 +13,12 @@ def estado(request):
 
 def index(request):
     return HttpResponse("index page")
+
+def projects(request):
+    proyectos = list(Project.objects.values())#.objects: Es un tipo administrador por defecto que crea Django, y a través de él, accedemos a los métodos para interactuar con la base de datos.
+    return JsonResponse(proyectos, safe=False)
+
+def tasks(request, id):#se recibe un id mediante la url que es recibido como un parametro en esta funcion
+    #task = Task.objects.get(id=id)#se hace la consulta en la base de datos
+    task =get_object_or_404(Task, id=id)
+    return HttpResponse("tasks: %s " %  task.title)
