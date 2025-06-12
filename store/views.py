cd70b1e0476cc8630e43e7fdb692e5a14d9bd363
favian silva <fabiansil902@gmail.com>
2025-06-12 17:16:37 -0500

    estuve viendo como hacer un bucle en el html para traer todos los datos que se tiene en la base de datos de esa funcion que preparamos en viesw

--- store/views.py
+++ store/views.py
@@ -15,7 +15,7 @@ def estado(request):
 def index(request):
     titulo = 'Django!!' #infromacion que queremos pasar al html 
     return render(request,'index.html', {'title': titulo})#aqui se utiliza la función render para mostrar la plantilla 'index.html'y el objeto 'request' contiene información sobre la solicitud web actual.
-    #el "{title:titulo} es para mostrar esa infromacion que tenemos en la variables de titulo pasarla a title y este lo pasamos al archivo html de index.html"
+    #el "{title:titulo} es para mostrar esa infomacion que tenemos en la variables de titulo pasarla a title y este lo pasamos al archivo html de index.html"
 
 def projects(request):
     #proyectos = list(Project.objects.values())#.objects: Es un tipo administrador por defecto que crea Django, y a través de él, accedemos a los métodos para interactuar con la base de datos.
