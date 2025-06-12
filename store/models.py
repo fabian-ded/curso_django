7819dac6f1389a8a31d114ad79e30f7d1a3ef310
favian silva <fabiansil902@gmail.com>
2025-06-12 17:45:10 -0500

    vi como se hacen condiciones y bucles en los html de mi aplicacion para asi mejorarla

--- store/models.py
+++ store/models.py
@@ -13,6 +13,8 @@ class Task(models.Model):
     description = models.TextField()#para textos mucho mas largos
     Project = models.ForeignKey(Project, on_delete=models.CASCADE)#Aqui se hace la relacion de las dos clases o mejor dicho de las dos tablas
     #ademas utilizamos la funcion de cascada que hace que si se elimina un proyecto se eliminen los datos que se tienen relacionados a ese
+    done = models.BooleanField(default=False)
+    
     
     def __str__(self):
         return self.title + ' - ' + self.Project.name #aqui se retorna el titulo y ademas se úede mostrar el nombre del proyecto de la otra tabla que esta relaccionada
